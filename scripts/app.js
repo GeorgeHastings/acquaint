1671e7dc7215595e2702a458cc2465d2629968dc
georgehastings <g.hastings3@gmail.com>
2016-07-26 17:05:53 -0400

    adding question demo page

--- scripts/app.js
+++ scripts/app.js
@@ -12,8 +12,18 @@ var UI = {
       UI.resourcesOpen = true;
     }
   },
+  repaint: function() {
+    var classname = '_loadin';
+    var elements = document.querySelectorAll('.'+classname);
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].classList.remove(classname);
+      console.log(elements[i].offsetWidth);
+      elements[i].classList.add(classname);
+    }
+  },
   bindEvents: function() {
     document.getElementById('resourcesLink').onclick = UI.toggleResources;
+    document.getElementById('nextQuestion').onclick = UI.repaint;
   }
 };
 
