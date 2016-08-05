8c7d1f5cb2d263599affb4e33a1c2621035437de
georgehastings <g.hastings3@gmail.com>
2016-08-05 11:31:30 -0400

    added assets

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var UI = {
+  scrolling: false,
+  currentTop: 0,
+  resourcesOpen: false,
   questionFlow: {
     signposts: document.querySelectorAll('.signpost li'),
     questions: document.querySelectorAll('.question'),
@@ -32,7 +35,6 @@ var UI = {
       UI.questionFlow.signposts[UI.questionFlow.index].classList.add('active');
     }
   },
-  resourcesOpen: false,
   toggleResources: function() {
     if(UI.resourcesOpen) {
       document.getElementById('dropdownResources').classList.remove('open');
@@ -41,6 +43,24 @@ var UI = {
     else {
       document.getElementById('dropdownResources').classList.add('open');
       UI.resourcesOpen = true;
+    }
+  },
+  autoHideHeader: function() {
+    var currentTop = document.body.scrollTop;
+    var scrollingDown = currentTop > UI.previousTop;
+    if(scrollingDown) {
+      document.getElementById('header').classList.add('hidden');
+    }
+    else {
+      document.getElementById('header').classList.remove('hidden');
+    }
+    UI.previousTop = currentTop;
+    UI.scrolling = false;
+  },
+  showHideHeader: function() {
+    if(!UI.scrolling) {
+      UI.scrolling = true;
+      (!window.requestAnimationFrame) ? setTimeout(UI.autoHideHeader, 250) : requestAnimationFrame(UI.autoHideHeader);
     }
   },
   repaint: function() {
@@ -60,9 +80,12 @@ var UI = {
   },
   bindEvents: function() {
     document.getElementById('resourcesLink').onclick = UI.toggleResources;
-    document.getElementById('nextQuestion').onclick = UI.questionFlow.nextQuestion;
-    document.getElementById('prevQuestion').onclick = UI.questionFlow.prevQuestion;
-    UI.bindToMany('input[type="radio"]', 'onchange', UI.questionFlow.enableButton);
+    window.onscroll = UI.showHideHeader;
+    if (document.getElementById('nextQuestion'), document.getElementById('prevQuestion')) {
+      document.getElementById('nextQuestion').onclick = UI.questionFlow.nextQuestion;
+      document.getElementById('prevQuestion').onclick = UI.questionFlow.prevQuestion;
+      UI.bindToMany('input[type="radio"]', 'onchange', UI.questionFlow.enableButton);
+    }
   }
 };
 
